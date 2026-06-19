c8f664396002b5a430b51530dca1bf407edb7279
Balakrishna Chennamsetti <balakrishna.chennamsetti@gmail.com>
2026-06-20 00:06:05 +0530

    fix: formated utils

--- src/main/gemma_system/utils.py
+++ src/main/gemma_system/utils.py
@@ -1,4 +1,9 @@
+import os
+
+from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
+
+load_dotenv()  # Load environment variables from .env file
 
 
 def llm_client(model_name: str = "gemma4:12b") -> ChatOllama:
@@ -11,6 +16,6 @@ def llm_client(model_name: str = "gemma4:12b") -> ChatOllama:
     ChatOllama client object
     """
     llm = ChatOllama(
-        model=model_name, temperature=0, base_url="http://host.docker.internal:11434"
+        model=model_name, temperature=0, base_url=os.getenv("GEMMA4_ENDPOINT")
     )
     return llm
